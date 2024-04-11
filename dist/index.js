35c95449ec7ccd0112c4f0bb4dec83b5e9b99a3c
Martelini <souza.mateus1998@gmail.com>
2024-04-10 21:31:44 -0300

    Template de conexão ao MongoDB

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = "mongodb+srv://souzamateus1998:EV5EGbftxv1fMPMx@cluster0.scduko7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
