f4d8d7aca42b6d5bf4ebb20e56809f8a669c4825
Martelini <souza.mateus1998@gmail.com>
2024-04-17 21:35:28 -0300

    APIs de posts criadas

--- controllers/smart.controllers.ts
+++ controllers/smart.controllers.ts
@@ -1,6 +1,10 @@
+// Imports
+
 import { RequestHandler } from "express";
 import { dbClient } from "../lib/mongo_connection";
-import { ObjectId } from "mongodb";
+import { ObjectId, Timestamp } from "mongodb";
+
+// Interfaces
 
 interface User {
     _id: ObjectId,
@@ -11,99 +15,20 @@ interface User {
     birthDate: { day: number, month: number, year: number }
 }
 
+interface Post {
+    _id: ObjectId,
+    accountId: ObjectId,
+    message: string,
+    timestamp: Timestamp
+}
+
+// Variables
+
 const database = dbClient.db('smart');
 const users = database.collection<User>('users');
+const posts = database.collection<Post>('posts');
 
-// welcomeMessage
-
-export const welcomeMessage: RequestHandler = async (req, res) => {
-    try {
-        res.send('Welcome to Smart!!!');
-    } catch (error) {
-        let message = 'Unknown error';
-        if (error instanceof Error) {
-            message = error.message;
-        }
-        res.status(500).json(message);
-    }
-}
-
-// signUp
-
-export const signUp: RequestHandler = async (req, res) => {
-    try {
-        const result = await users.insertOne(req.body);
-        res.status(200).json(result);
-    } catch (error) {
-        let message = 'Unknown error';
-        if (error instanceof Error) {
-            message = error.message;
-        }
-        res.status(500).json({ message });
-    }
-}
-
-// Get all users
-
-export const getAllUsers: RequestHandler = async (req, res) => {
-    try {
-        const result = await users.find().toArray();
-        if (result) {
-            res.status(200).json(result);
-        }
-        else {
-            res.status(404).json('None user found!');
-        }
-    } catch (error) {
-        let message = 'Unknown error';
-        if (error instanceof Error) {
-            message = error.message;
-        }
-        res.status(500).json(message);
-    }
-}
-
-// Get one user by ID
-
-export const getUser: RequestHandler = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const result = await users.findOne({ '_id': new ObjectId(userId) });
-        if (result) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({ message: 'User could not be found!' });
-        }
-    } catch (error) {
-        let message = 'Unknown error';
-        if (error instanceof Error) {
-            message = error.message;
-        }
-        res.status(500).json(message);
-    }
-}
-
-// Update user
-
-export const updateUser: RequestHandler = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const result = await users.updateOne({ '_id': new ObjectId(userId) }, { $set: req.body });
-        if (result) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({ message: 'User could not be found!' });
-        }
-    } catch (error) {
-        let message = 'Unknown error';
-        if (error instanceof Error) {
-            message = error.message;
-        }
-        res.status(500).json(message);
-    }
-}
-
-// Delete user
+// Local functions
 
 function checkError(error: any): string {
     if (error instanceof Error) {
@@ -112,8 +37,89 @@ function checkError(error: any): string {
     return 'Unknown error';
 }
 
+// External functions
+
+// welcomeMessage
+
+export const welcomeMessage: RequestHandler = async (req, res) => {
+    try {
+        console.log('welcomeMessage');
+        res.send('Welcome to Smart!!!');
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Users functions //
+
+// signUp
+
+export const signUp: RequestHandler = async (req, res) => {
+    try {
+        console.log('signUp');
+        const result = await users.insertOne(req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Get all users
+
+export const getAllUsers: RequestHandler = async (req, res) => {
+    try {
+        console.log('getAllUsers');
+        const result = await users.find().toArray();
+        if (result) {
+            res.status(200).json(result);
+        }
+        else {
+            res.status(404).json('None user found!');
+        }
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Get one user by ID
+
+export const getUser: RequestHandler = async (req, res) => {
+    try {
+        console.log('getUser');
+        const { userId } = req.params;
+        const result = await users.findOne({ '_id': new ObjectId(userId) });
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'User could not be found!' });
+        }
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Update user
+
+export const updateUser: RequestHandler = async (req, res) => {
+    try {
+        console.log('updateUser');
+        const { userId } = req.params;
+        const result = await users.updateOne({ '_id': new ObjectId(userId) }, { $set: req.body });
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'User could not be found!' });
+        }
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Delete user
+
 export const deleteUser: RequestHandler = async (req, res) => {
     try {
+        console.log('deleteUser');
         const { userId } = req.params;
         const result = await users.deleteOne({ '_id': new ObjectId(userId) });
         if (result) {
@@ -121,6 +127,65 @@ export const deleteUser: RequestHandler = async (req, res) => {
         } else {
             res.status(404).json({ message: 'User could not be found!' });
         }
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Post functions //
+
+// Create post
+
+export const createPost: RequestHandler = async (req, res) => {
+    try {
+        console.log('createPost');
+        const { userId } = req.params;
+        req.body.accountId = new ObjectId(userId);
+        req.body.timestamp = Date.now();
+        const result = await posts.insertOne(req.body);
+        if (result) {
+            res.status(200).json({ message: 'Posted successfully', result: result });
+        } else {
+            res.status(500).json({ message: 'Could not create post' });
+        }
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Read all posts (limited to 20)
+
+export const readAllPosts: RequestHandler = async (req, res) => {
+    try {
+        console.log('readAllPosts');
+        const result = await posts.find().toArray();
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).send('None posts were found!');
+        }            
+    } catch (error) {
+        res.status(500).json(checkError(error));
+    }
+}
+
+// Read all posts of a user
+
+export const readAllUserPosts: RequestHandler = async (req, res) => {
+    try {
+        console.log('readAllUserPosts');
+        const { userId } = req.params;
+        const user = await users.findOne({ _id: new ObjectId(userId) });
+        if (user) {
+            const result = await posts.find({ accountId: new ObjectId(userId) }).toArray();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).send('None posts were found!');
+            }
+        } else {
+            res.status(404).send('User not found!');
+        }            
     } catch (error) {
         res.status(500).json(checkError(error));
     }
--- routes/smart.routes.ts
+++ routes/smart.routes.ts
@@ -1,4 +1,4 @@
-import { deleteUser, getAllUsers, getUser, signUp, updateUser, welcomeMessage } from '../controllers/smart.controllers'
+import { createPost, deleteUser, getAllUsers, getUser, readAllPosts, readAllUserPosts, signUp, updateUser, welcomeMessage } from '../controllers/smart.controllers'
 import { Router } from "express";
 export const router = Router();
 
@@ -8,17 +8,29 @@ router.get('/home', welcomeMessage);
 
 // Read users
 
-router.get('/getUsers', getAllUsers);
-router.get('/:userId', getUser);
+router.get('/users/all', getAllUsers);
+router.get('/users/:userId', getUser);
 
 // Add users
 
-router.post('/signUp', signUp);
+router.post('/users/signUp', signUp);
 
 // Update user
 
-router.put('/update/:userId', updateUser);
+router.put('/users/update/:userId', updateUser);
 
 // Delete user
 
-router.delete('/delete/:userId', deleteUser);
+router.delete('/users/delete/:userId', deleteUser);
+
+// Create post
+
+router.post('/users/:userId/createPost', createPost);
+
+// Read all posts
+
+router.get('/posts/all', readAllPosts);
+
+// Read all posts of a user
+
+router.get('/users/:userId/posts', readAllUserPosts);
